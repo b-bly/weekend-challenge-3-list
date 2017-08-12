96962756a161f966c9a8f04f8770c5e0d352855b
Brendt Bly <brendt.bly@outlook.com>
2017-08-12 15:53:02 -0500

    completed button styled

--- server/routes/list.js
+++ server/routes/list.js
@@ -65,25 +65,4 @@ router.post('/deleteItems', function (req, res) {
 });
 
 
-// router.put('/', function (req, res) {
-//     console.log('message put was hit!');
-//     pool.connect(function (errorConnectingToDatabase, client, done) {
-//         done();
-//         if (errorConnectingToDatabase) {
-//             console.log('Error connecting to database: ', errorConnectingToDatabase);
-//             res.sendStatus(500);
-//         } else {
-//             client.query('UPDATE messages SET message=$1 WHERE id=$2', [req.body.message, req.body.id], function (errorMakingQuery, result) {
-//                 if (errorMakingQuery) {
-//                     UPDATE books SET author = ‘Sussanna Clarke’, title=‘Jonathan Strange and Mr. Norrel’ WHERE id=‘9’;
-//                     console.log('Error making database query: ', errorMakingQuery);
-//                     res.sendStatus(500);
-//                 } else {
-//                     res.sendStatus(200);
-//                 }
-//             });
-//         }
-//     });
-// });
-
 module.exports = router;
